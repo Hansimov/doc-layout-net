05ac394c64d8bafc9c6e815bf1747dc078235f07
Hansimov <591172499@qq.com>
2024-02-21 01:08:39 +0800

    :zap: [Enhance] Predictor: Auto use latest weights, and handle checkpoint states

--- training/doc_element_detect_predictor.py
+++ training/doc_element_detect_predictor.py
@@ -26,11 +26,22 @@ class DocElementDetectPredictor:
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.num_classes = NUM_CLASSES
 
+    def get_latest_weights_path(self):
+        weights_paths = list(CHECKPOINTS_ROOT.glob("*.pth"))
+        weights_paths = sorted(
+            weights_paths, key=lambda x: x.stat().st_mtime, reverse=True
+        )
+        latest_weights_path = weights_paths[0]
+        return latest_weights_path
+
     def load_weights(self, weights_path):
-        self.weights_path = weights_path
-        self.weights = torch.load(weights_path)
         self.model = fasterrcnn_resnet50_fpn(num_classes=self.num_classes)
-        self.model.load_state_dict(self.weights)
+        state_dict = torch.load(weights_path)
+        if weights_path.stem.startswith("checkpoint"):
+            weights = state_dict["model_state_dict"]
+        else:
+            weights = state_dict
+        self.model.load_state_dict(weights)
         self.model.to(self.device)
         self.model.eval()
 
@@ -46,6 +57,7 @@ class DocElementDetectPredictor:
             map(lambda x: x[scores > threshold], [boxes, labels, scores])
         )
         boxes, labels, scores = list(map(lambda x: x.tolist(), [boxes, labels, scores]))
+        boxes = [list(map(lambda x: round(x), box)) for box in boxes]
 
         # predict results as dict, and dump to json
         predict_results = []
@@ -65,9 +77,8 @@ class DocElementDetectPredictor:
 
         for idx, predict_result in enumerate(predict_results):
             box, label, score = list(map(predict_result.get, ["box", "label", "score"]))
-            box = denormalize_x1y1x2y2(box, *image.size)
             category = CATEGORY_NAMES[label]
-            logger.line(f"  - {category} ({round(score,2)}): {box}")
+            logger.line(f"  - {idx+1}.{category} <{label}> ({round(score,2)}): {box}")
             color = CATEGORY_COLORS[category]
             rect_box = x1y1x2y2_with_spacing(box, spacing=bbox_spacing)
             draw.rectangle(rect_box, outline=color, fill=(*color, 64), width=2)
@@ -76,7 +87,6 @@ class DocElementDetectPredictor:
         text_font = ImageFont.truetype("times.ttf", 15)
         for idx, predict_result in enumerate(predict_results):
             box, label, score = list(map(predict_result.get, ["box", "label", "score"]))
-            box = denormalize_x1y1x2y2(box, *image.size)
             category_name = CATEGORY_NAMES[label]
             if category_name in CATEGORY_NEW_NAMES:
                 category_name = CATEGORY_NEW_NAMES[category_name]
@@ -90,7 +100,9 @@ class DocElementDetectPredictor:
             )
         return image
 
-    def predict(self, image_path, weights_path, threshold=0.1):
+    def predict(self, image_path, weights_path=None, threshold=0.5):
+        if not weights_path:
+            weights_path = self.get_latest_weights_path()
         logger.note(f"> Loading weights from: {weights_path}")
         self.load_weights(weights_path)
         logger.note(f"> Predicting image: {image_path}")
@@ -99,7 +111,6 @@ class DocElementDetectPredictor:
         image_tensor = self.image_to_tensor(image_path)
         with torch.no_grad():
             prediction = self.model([image_tensor])[0]
-            logger.mesg(prediction)
         predict_results = self.prediction_to_dict_list(prediction, threshold)
 
         # save predict results to json
@@ -120,19 +131,17 @@ class DocElementDetectPredictor:
 if __name__ == "__main__":
     with Runtimer():
         predictor = DocElementDetectPredictor()
+        image_pattern = "train_*.jpg"
         image_paths = sorted(
             [
                 path
-                for path in SAMPLES_ROOT.glob("image_worth_*.png")
+                for path in SAMPLES_ROOT.glob(image_pattern)
                 if not path.stem.endswith("_predict")
             ]
         )
-
         image_path = image_paths[0]
-        weights_name = "weights_pq-1_sd-1_ep-1_bs-16_lr-1e-05-auto.pth"
+        weights_name = "weights_pq-1_sd-None_ep-300_bs-1_lr-0.001-auto.pth"
         weights_path = CHECKPOINTS_ROOT / weights_name
-        predictor.predict(
-            image_path=image_path, weights_path=weights_path, threshold=0.2
-        )
+        predictor.predict(image_path=image_path, threshold=0.5)
 
     # python -m training.doc_element_detect_predictor
